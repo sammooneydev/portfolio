50599387771cc1088824ca30538ac827b875f71b
Sam Mooney <sammooneydev@gmail.com>
2025-08-10 17:30:10 +0100

    added gradient effect on heading text within TheHeader vue component, also added nice fading in animation for profile cards, as well as some simple text within the cards

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,46 +1,69 @@
 <template>
     <div class="card">
-        <p>still a work in progress!</p>
+        <h2>About Me</h2>
+        <p>My name is Sam Mooney, and welcome to my portfolio site! 
+           I am a 4th year student currently studying computing at Abertay University</p>
     </div>
 </template>
 
 <style scoped>
+/*import for heading font*/
+@import url('https://fonts.googleapis.com/css2?family=Archivo+Black&family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Space+Grotesk:wght@300..700&display=swap');
+
+
+h2 {
+    margin-bottom: 15px;
+    font-family: "Archivo Black", sans-serif;
+}
+
+@keyframes fadeUp {
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+
 @media (prefers-color-scheme: dark) {
     .card{
-        width: 320px;
-        height: 180px;
+        width: 500px;
+        height: 250px;
         background-image: url('../assets/retro-card-design-dark.svg');
         background-size:100% 100%;
         background-position: center;
         display: flex;
         flex-direction: column;
-        justify-content: center;
+        justify-content: top;
         align-items: center;
         box-shadow: 0 4px 16px rgba(65,56,50, 0.30);
         border-radius: 3px;
         padding: 1.5rem;
         color: #f4eade;
         text-align: center;
+        opacity: 0;
+        transform: translateY(20px);
+        animation: fadeUp 1s ease-out forwards;
     }
 }
 
 @media (prefers-color-scheme: light) {
     .card {
-        width: 320px;
-        height: 180px;
+        width: 500px;
+        height: 250px;
         background-image: url('../assets/retro-card-design.svg');
         background-size:100% 100%;
         background-position: center;
         display: flex;
         flex-direction: column;
-        justify-content: center;
+        justify-content: top;
         align-items: center;
         box-shadow: 0 4px 16px rgba(0,0,0,0.18);
         border-radius: 3px;
         padding: 1.5rem;
         color: #2b2018;
         text-align: center;
+        opacity: 0;
+        transform: translateY(20px);
+        animation: fadeUp 1s ease-out forwards;
     }
 }
-
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -9,11 +9,14 @@
 @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap');
 
 h1 {
-  color: #c21717;
   text-align: center;
   margin: 0;
   padding: 5px;
   font-weight: bold;
   font-family: 'Space Grotesk', sans-serif;
+  background: linear-gradient(90deg,#c21717, #e76219, #fea712);
+  -webkit-background-clip: text;
+  color: transparent;
+  display: inline-block;
 }
 </style>
