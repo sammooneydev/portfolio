189427f7aa0d5ec4a5b00f20fe305062894bb586
Sam Mooney <sammooneydev@gmail.com>
2025-08-16 00:39:34 +0100

    added more basic styling to NavBar vue component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,8 @@
 <template>
     <nav class="nav-bar">
       <RouterLink to="/">HOME</RouterLink>
+      <RouterLink to="/projects">PROJECTS</RouterLink>
+      <RouterLink to="/contact">CONTACT</RouterLink>
     </nav>
 </template>
 
@@ -8,18 +10,23 @@
 .nav-bar {
   margin-bottom: 10px;
   padding: 10px;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
 }
 
 .nav-bar a {
   list-style: none;
   padding: 0;
   margin: 0;
-  display: flex;
-  justify-content: center;
-  gap: 10px;
   text-decoration: none;
   color: #c21717;
   font-family: "Space Grotesk", sans-serif;
   font-weight: bold;
+}
+
+.nav-bar a.router-link-exact-active {
+    background-color: #fea712;
+    border-radius: 4px;
 }
 </style>
