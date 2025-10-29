cbfe001e41ac09abfc183479f5c0670756897b46
sam mooney <sammooneydev@gmail.com>
2025-10-29 01:15:14 +0000

    added projects.js file for use in exporting project information to be displayed on page

---
+++ src/data/projects.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    title: "Gap or Cap",
+    description: "Small-scale project which serves to translate an in-joke from me and my friends into a discord bot.",
+    repo_url: "https://github.com/sammooneydev/gap-or-cap",
+    languages: ["JavaScript"],
+  },
+];
