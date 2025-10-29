a14acdc0dce24830303ed0760e4b10904758b160
sam mooney <sammooneydev@gmail.com>
2025-10-29 02:20:46 +0000

    implemented initial version of ProjectItem.vue component without styling (that will be added soon)

---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+    title: String,
+    description: String,
+    repo_url: String,
+    languages: Array,
+})
+</script>
+
+<template>
+  <div class="project-item-background">
+    <div class="project-item">
+        <h4>{{ title }}</h4>
+        <div class="project-languages">
+            <span v-for="language in languages" :key="language">
+                {{ language }}
+            </span>    
+        </div>
+        <p>{{ description }}</p>
+        <a :href="repo_url" target="_blank">
+            <img src="../assets/github-mark-white.svg" alt="Github icon" />
+        </a>
+    </div>
+  </div>
+</template>
