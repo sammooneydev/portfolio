e8790ef79366f03e6ca458c6637bdde5afd81298
sam mooney <sammooneydev@gmail.com>
2025-10-30 10:09:52 +0000

    added social links to profile section, those being github profile and linkedin profile

--- src/components/ProfileSection.vue
+++ src/components/ProfileSection.vue
@@ -12,6 +12,23 @@
         I'm currently working on my honours project, which aims to be a search
         engine for technical resources.
       </p>
+      <h4># links</h4>
+      <div class="socials">
+        <a href="https://github.com/sammooneydev">
+          <img
+            src="../assets/github-mark-white.svg"
+            alt="Github icon"
+            class="icon"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/sam-mooney-b8254632b">
+          <img
+            src="../assets/linkedin-app-white-icon.svg"
+            alt="Linkedin icon"
+            class="icon"
+          />
+        </a>
+      </div>
     </div>
   </section>
 </template>
@@ -22,7 +39,7 @@ h4 {
   margin-bottom: 0.5rem;
   margin-top: 0;
   opacity: 0.7;
-  color:#f9a825
+  color: #f9a825;
 }
 
 p {
@@ -54,5 +71,19 @@ p {
   border-width: 0.1rem;
   border-radius: 0.3rem;
   border-color: #111927;
+}
+
+.icon {
+  width: 30px;
+  height: 30px;
+}
+
+.socials {
+  display: flex;
+  gap: 1rem;
+  margin-top: 1rem;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
 }
 </style>
