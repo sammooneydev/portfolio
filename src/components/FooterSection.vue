e5d721538518a5abfd81be4fe32a8e80532d1885
sam mooney <sammooneydev@gmail.com>
2025-11-03 15:58:07 +0000

    finished footer section, with email contact as well as accrediting what site i used to create the ascii banner

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,20 +1,41 @@
+<script setup>
+import { ref } from "vue";
+
+const showTooltip = ref(false);
+
+function copyEmail() {
+  navigator.clipboard.writeText("sammooneydev@gmail.com");
+  showTooltip.value = true;
+  setTimeout(() => (showTooltip.value = false), 1500);
+}
+</script>
+
 <template>
   <div class="footer">
-    
-
-    <p>ascii banner created using the <a href="https://manytools.org/hacker-tools/ascii-banner/">many tools page for ascii banner</a></p>
-    <p>// end of page :) </p>
+    <p>
+      * ascii banner created using the
+      <a href="https://manytools.org/hacker-tools/ascii-banner/"
+        >many tools page for ascii banner</a
+      >
+    </p>
+    <p>
+      * for serious enquiries:
+      <span class="email" @click="copyEmail">
+        sammooneydev@gmail.com
+        <span v-if="showTooltip" class="tooltip">copied!</span>
+      </span>
+    </p>
+    <p>--- end of portfolio ---</p>
   </div>
 </template>
 
 <style scoped>
-
 .footer {
-    align-items: center;
-    justify-content: center;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 }
 
 p {
@@ -34,4 +55,19 @@ a:hover {
   text-decoration: underline;
 }
 
+.email {
+  color: #f9a825;
+  font-weight: bold;
+  cursor: pointer;
+  display: inline-block;
+}
+
+.email:hover {
+  text-decoration: underline;
+}
+
+.tooltip {
+  color: var(--fg);
+  font-size: 0.75rem;
+}
 </style>
