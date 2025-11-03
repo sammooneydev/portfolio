9435edde3f02eb17c9c23bcbc8df22cb0249dd73
sam mooney <sammooneydev@gmail.com>
2025-11-03 12:43:40 +0000

    updated footer component

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,7 +1,37 @@
 <template>
   <div class="footer">
-    <p>end of page :) </p>
+    
+
+    <p>ascii banner created using the <a href="https://manytools.org/hacker-tools/ascii-banner/">many tools page for ascii banner</a></p>
+    <p>// end of page :) </p>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+.footer {
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+}
+
+p {
+  font-size: 0.8rem;
+  margin-top: 0;
+  margin-bottom: 0;
+  line-height: 1.5;
+}
+
+a {
+  color: #f9a825;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+</style>
